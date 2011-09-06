232573b0df37f4bafa718e1e764df94f325c2330
Jason Dillon <jason@planet57.com>
2011-09-06 15:30:22 -0700

    Move to package

--- litmus-testsupport/src/main/java/org/sonatype/sisu/litmus/testsupport/mock/ImplementsPartialJavaBean.java
+++ litmus-testsupport/src/main/java/org/sonatype/sisu/litmus/testsupport/mock/ImplementsPartialJavaBean.java
@@ -10,7 +10,8 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package org.sonatype.sisu.litmus.testsupport;
+
+package org.sonatype.sisu.litmus.testsupport.mock;
 
 import com.google.common.collect.Maps;
 import org.mockito.internal.stubbing.defaultanswers.ReturnsMoreEmptyValues;
