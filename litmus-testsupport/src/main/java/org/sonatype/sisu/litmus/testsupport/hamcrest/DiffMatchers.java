e4d4a250f1fa0b95cd14bd8bb4c8e994c18dd8c6
Alin Dreghiciu <adreghiciu@gmail.com>
2011-10-05 20:33:54 +0300

    Extract utility methods to another class to no confuse user with static methods. Extends BaseMatcher so we can handle nulls. Add line numbers. Add a first column marker [A]dded/[D]eleted/[M]odified

--- litmus-testsupport/src/main/java/org/sonatype/sisu/litmus/testsupport/hamcrest/DiffMatchers.java
+++ litmus-testsupport/src/main/java/org/sonatype/sisu/litmus/testsupport/hamcrest/DiffMatchers.java
@@ -13,15 +13,10 @@
 
 package org.sonatype.sisu.litmus.testsupport.hamcrest;
 
-import java.util.List;
-
+import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.incava.util.diff.Diff;
-import org.incava.util.diff.Difference;
-import com.google.common.base.Strings;
 
 /**
  * Some ideas to represent test assertions in more usable way
@@ -33,194 +28,70 @@ import com.google.common.base.Strings;
  *
  * @author cstamas
  */
-public class DiffMatchers
-{
+public class DiffMatchers {
 
     private static class StringDiffMatcher
-        extends TypeSafeMatcher<String>
-    {
+            extends BaseMatcher<String> {
 
         private final String string;
 
-        private String item;
+        private boolean onlyDiffs;
 
-        public StringDiffMatcher( final String string )
-        {
+        public StringDiffMatcher(final String string) {
+            this(string, false);
+        }
+
+        public StringDiffMatcher(final String string, final boolean onlyDiffs) {
             this.string = string;
+            this.onlyDiffs = onlyDiffs;
         }
 
         @Override
-        protected boolean matchesSafely( final String item )
-        {
-            this.item = item;
-
-            if ( item == null )
-            {
+        public boolean matches(Object arg) {
+            if (arg == null) {
                 return string == null;
-            }
-            else
-            {
-                return item.equals( string );
+            } else {
+                return arg.equals(string);
             }
         }
 
         @Override
-        public void describeTo( final Description description )
-        {
-            final String diff = diffSideBySide( string, item );
-
-            description.appendText( "\n" ).appendText( diff );
+        public void describeTo(final Description description) {
+            description.appendText("to be equal");
         }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        final public void describeMismatch(Object item, Description description) {
+            if (item == null) {
+                super.describeMismatch(item, description);
+            } else if (!String.class.isInstance(item)) {
+                description.appendText("was a ")
+                        .appendText(item.getClass().getName())
+                        .appendText(" (")
+                        .appendValue(item)
+                        .appendText(")");
+            } else {
+                describeMismatchSafely((String) item, description);
+            }
+        }
+
+        protected void describeMismatchSafely(String item, Description description) {
+            description.appendText("they differ as follows (expected on the left, actual on the right):\n");
+            final String diff = DiffUtils.diffSideBySide(string, item, onlyDiffs);
+            description.appendText(diff);
+        }
+
     }
 
     @Factory
-    public static Matcher<String> equalTo( final String string )
-    {
-        return new StringDiffMatcher( string );
+    public static Matcher<String> equalTo(final String string) {
+        return new StringDiffMatcher(string);
     }
 
-    // ==
-
-    public static int maxLength( String... lines )
-    {
-        int maxLength = 0;
-
-        for ( String line : lines )
-        {
-            if ( maxLength < line.length() )
-            {
-                maxLength = line.length();
-            }
-        }
-        return maxLength;
+    @Factory
+    public static Matcher<String> equalToOnlyDiffs(final String string) {
+        return new StringDiffMatcher(string, true);
     }
 
-    public static String diffSideBySide( String fromStr, String toStr )
-    {
-        // this is equivalent of running unix diff -y command
-        // not pretty, but it works. Feel free to refactor against unit test.
-        String[] fromLines = fromStr.split( "\n" );
-        String[] toLines = toStr.split( "\n" );
-        List<Difference> diffs = ( new Diff( fromLines, toLines ) ).diff();
-
-        int padding = 3;
-        int maxStrWidth = Math.max( maxLength( fromLines ), maxLength( toLines ) ) + padding;
-
-        StringBuilder diffOut = new StringBuilder();
-        int fromLineNum = 0;
-        int toLineNum = 0;
-        for ( Difference diff : diffs )
-        {
-            int delStart = diff.getDeletedStart();
-            int delEnd = diff.getDeletedEnd();
-            int addStart = diff.getAddedStart();
-            int addEnd = diff.getAddedEnd();
-
-            boolean isAdd = ( delEnd == Difference.NONE && addEnd != Difference.NONE );
-            boolean isDel = ( addEnd == Difference.NONE && delEnd != Difference.NONE );
-            boolean isMod = ( delEnd != Difference.NONE && addEnd != Difference.NONE );
-
-            //write out unchanged lines between diffs
-            while ( true )
-            {
-                String left = "";
-                String right = "";
-                if ( fromLineNum < ( delStart ) )
-                {
-                    left = fromLines[fromLineNum];
-                    fromLineNum++;
-                }
-                if ( toLineNum < ( addStart ) )
-                {
-                    right = toLines[toLineNum];
-                    toLineNum++;
-                }
-                diffOut.append( Strings.padEnd( left, maxStrWidth, ' ' ) );
-                diffOut.append( "  " ); // no operator to display
-                diffOut.append( right ).append( "\n" );
-
-                if ( ( fromLineNum == ( delStart ) ) && ( toLineNum == ( addStart ) ) )
-                {
-                    break;
-                }
-            }
-
-            if ( isDel )
-            {
-                //write out a deletion
-                for ( int i = delStart; i <= delEnd; i++ )
-                {
-                    diffOut.append( Strings.padEnd( fromLines[i], maxStrWidth, ' ' ) );
-                    diffOut.append( "<" ).append( "\n" );
-                }
-                fromLineNum = delEnd + 1;
-            }
-            else if ( isAdd )
-            {
-                //write out an addition
-                for ( int i = addStart; i <= addEnd; i++ )
-                {
-                    diffOut.append( Strings.padEnd( "", maxStrWidth, ' ' ) );
-                    diffOut.append( "> " );
-                    diffOut.append( toLines[i] ).append( "\n" );
-                }
-                toLineNum = addEnd + 1;
-            }
-            else if ( isMod )
-            {
-                // write out a modification
-                while ( true )
-                {
-                    String left = "";
-                    String right = "";
-                    if ( fromLineNum <= ( delEnd ) )
-                    {
-                        left = fromLines[fromLineNum];
-                        fromLineNum++;
-                    }
-                    if ( toLineNum <= ( addEnd ) )
-                    {
-                        right = toLines[toLineNum];
-                        toLineNum++;
-                    }
-                    diffOut.append( Strings.padEnd( left, maxStrWidth, ' ' ) );
-                    diffOut.append( "| " );
-                    diffOut.append( right ).append( "\n" );
-
-                    if ( ( fromLineNum > ( delEnd ) ) && ( toLineNum > ( addEnd ) ) )
-                    {
-                        break;
-                    }
-                }
-            }
-
-        }
-
-        //we've finished displaying the diffs, now we just need to run out all the remaining unchanged lines
-        while ( true )
-        {
-            String left = "";
-            String right = "";
-            if ( fromLineNum < ( fromLines.length ) )
-            {
-                left = fromLines[fromLineNum];
-                fromLineNum++;
-            }
-            if ( toLineNum < ( toLines.length ) )
-            {
-                right = toLines[toLineNum];
-                toLineNum++;
-            }
-            diffOut.append( Strings.padEnd( left, maxStrWidth, ' ' ) );
-            diffOut.append( "  " ); // no operator to display
-            diffOut.append( right ).append( "\n" );
-
-            if ( ( fromLineNum == ( fromLines.length ) ) && ( toLineNum == ( toLines.length ) ) )
-            {
-                break;
-            }
-        }
-
-        return diffOut.toString();
-    }
 }
